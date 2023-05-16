519ad572e7fc8b02699a75a6fb2915cd213383a9
Rolf Geschke <71587437+Natorden@users.noreply.github.com>
2023-05-16 14:00:33 +0200

    added controller

---
+++ DLS/sender-api/Controllers/SenderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SenderAPI.Models;
+
+namespace SenderAPI.Controllers;
+
+public class SenderController : Controller
+{
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] Messages messages)
+    {
+        return null;
+    }
+}
---
+++ DLS/sender-api/Models/Messages.cs
@@ -0,0 +1,7 @@
+﻿namespace SenderAPI.Models;
+
+public class Messages
+{
+    public int Id { get; set; }
+    public string Message { get; set; }
+}
