6f7701ac2dada82248c701dc39b12e68b2e90791
Natorden <71587437+Natorden@users.noreply.github.com>
2023-05-17 13:50:48 +0200

    tracing attempt

--- DLS/sender-api/Program.cs
+++ DLS/sender-api/Program.cs
@@ -1,25 +1,57 @@
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddOpenTelemetryTracing(b => b
+    .SetResourceBuilder(ResourceBuilder.CreateDefault()
+        .AddService("SenderAPI"))
+    .AddAspNetCoreInstrumentation()
+    .AddHttpClientInstrumentation()
+    .AddMyConsoleExporter()
+);
+
+// Add support for tracing
+builder.Services.AddOpenTelemetryTracing(b => b
+    .SetResourceBuilder(ResourceBuilder
+        .CreateDefault().AddService("SenderAPI"))
+    .AddAspNetCoreInstrumentation()
+    .AddHttpClientInstrumentation()
+    .AddConsoleExporter()
+);
+
+// Add support for metrics
+builder.Services.AddOpenTelemetryMetrics(b => b
+    .SetResourceBuilder(ResourceBuilder
+        .CreateDefault().AddService("SenderAPI"))
+    .AddAspNetCoreInstrumentation()
+    .AddConsoleExporter()
+);
+
+// Configure logging
+builder.Logging.AddOpenTelemetry(options =>
+    {
+        options.IncludeFormattedMessage = true;
+        options.SetResourceBuilder(ResourceBuilder.CreateDefault()
+            .AddService("SenderAPI"));
+        options.AddConsoleExporter();
+    });
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-//app.UseHttpsRedirection();
-
+// Configure the HTTP request pipeline
+app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
