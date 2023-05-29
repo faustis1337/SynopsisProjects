a4277d627d164cb46e4ae0956cbe61b1fbacc0e5
Rolf Geschke <71587437+Natorden@users.noreply.github.com>
2023-05-29 11:32:43 +0200

    DBD

---
+++ DBD/NoSql/Controllers/NoSqlController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NoSql.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NoSqlController : ControllerBase
+{
+}
+
---
+++ DBD/Sql/Controllers/SqlController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sql.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SqlController : ControllerBase
+{
+}
