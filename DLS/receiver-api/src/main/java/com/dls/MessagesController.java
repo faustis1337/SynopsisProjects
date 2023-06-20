ad8a8597a1ebf91916626119e00b45fdf8dbc2bb
faustis1337 <faustis1337@gmail.com>
2023-06-20 07:48:03 +0200

    logger

--- DLS/receiver-api/src/main/java/com/dls/MessagesController.java
+++ DLS/receiver-api/src/main/java/com/dls/MessagesController.java
@@ -2,6 +2,8 @@ package com.dls;
 
 import com.dls.Entities.MessageRequest;
 import com.dls.Repositories.MessageRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ import java.util.List;
 public class MessagesController {
 
     private final MessageRepository messageRepository = new MessageRepository();
+    private static final Logger logger = LoggerFactory.getLogger(MessagesController.class);
 
     public static void main(String[] args) {
         SpringApplication.run(MessagesController.class, args);
@@ -22,9 +25,11 @@ public class MessagesController {
 
     @PostMapping
     public MessageRequest newMessage(@RequestBody MessageRequest request) {
+        logger.info("Creating a new message");
         String id = request.getId();
         String message = request.getMessage();
         if (id != null && message != null) {
+            logger.info("Storing message");
             messageRepository.add(request);
         }
         return request;
@@ -32,6 +37,7 @@ public class MessagesController {
 
     @GetMapping
     public List<MessageRequest> getAllMessages() {
+        logger.info("Getting all messages");
         return messageRepository.getAllMessages();
     }
 }
